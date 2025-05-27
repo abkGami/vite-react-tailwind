39594ce6dc2f1e3c8683123ff48d5656da15f333
abkGami <yahabubakar1@gmail.com>
2025-05-27 17:44:37 +0100

    styled using tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <span>This is a vite+react website with tailwind installed</span>
+      <div className=" flex items-center justify-center min-h-screen bg-gray-100">
+        <span className="text-red-500 text-xl font-bold">
+          This is a vite+react website with tailwind installed
+        </span>
       </div>
     </>
   );
