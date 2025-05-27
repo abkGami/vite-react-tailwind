5147186260e9e843d98d8c48b087aff6d1c66194
abkGami <yahabubakar1@gmail.com>
2025-05-27 18:03:57 +0100

    turning it to a package

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <>
       <div className=" flex items-center justify-center min-h-screen bg-gray-100">
         <span className="text-red-500 text-xl font-bold">
-          This is a vite+react website with tailwind installed
+          This is a vite+react website with tailwind installed for styling
         </span>
       </div>
     </>
